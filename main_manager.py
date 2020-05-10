2ae32a0b4f529636064a3c1e1563e743b65cd181
MattMuelot <mattmuelot@gmail.com>
2020-05-10 11:53:39 -0700

    Creating code structure

--- main_manager.py
+++ main_manager.py
@@ -13,8 +13,16 @@ class Mangler:
     def new_branch_name(self, branch_type):
         if branch_type in self.accepted_branch_types:
             if branch_type == 'feature':
-                pass
+                new_branch = f'feature-{random.randint(1, 1000)}'
+                if new_branch in self.working_branches:
+                    return False
+                else:
+                    self.working_branches.append(new_branch)
             elif branch_type == 'bug':
-                pass
+                new_branch = f'bug-{random.randint(1, 10000)}'
+                if new_branch in self.working_branches:
+                    return False
+                else:
+                    self.working_branches.append(new_branch)
         else:
             return False
