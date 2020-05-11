4440cfccb9f30d073f9a77d50891bf49dd6b8f29
MattMuelot <mattmuelot@gmail.com>
2020-05-11 11:43:48 -0700

    Branch name generation

--- main_manager.py
+++ main_manager.py
@@ -1,35 +1,35 @@
-import random
+import random as r
+from string import ascii_letters
 
 
 class Mangler:
     def __init__(self, proj_name):
         self.proj_name = proj_name
-        self.working_branches = []
+        self.feature_branches = []
+        self.bug_branches = []
         self.accepted_branch_types = ['feature', 'bug']
 
     def __repr__(self):
         return f"Mangler('{self.proj_name}')"
 
     def new_branch_name(self, branch_type):
-        # TODO: Add loop so method restarts if branch already exists
-        if branch_type in self.accepted_branch_types:
-            if branch_type == 'feature':
-                new_branch = f'feature-{random.randint(1, 1000)}'
-                if new_branch in self.working_branches:
-                    return False
-                else:
-                    self.working_branches.append(new_branch)
-            elif branch_type == 'bug':
-                new_branch = f'bug-{random.randint(1, 10000)}'
-                if new_branch in self.working_branches:
-                    return False
-                else:
-                    self.working_branches.append(new_branch)
-        else:
-            return False
-
-    def remove_branch_by_name(self, branch_name):
-        if branch_name in self.working_branches:
-            self.working_branches.remove(branch_name)
-        else:
-            return False
+        while True:
+            if branch_type in self.accepted_branch_types:
+                if branch_type == 'feature':
+                    b_id = r.sample(ascii_letters, 2)
+                    new_branch = f'feature-{b_id[0]}{b_id[1]}{r.randint(10, 99)}{r.choice(ascii_letters)}'
+                    if new_branch in self.feature_branches:
+                        continue
+                    else:
+                        self.feature_branches.append(new_branch)
+                        return new_branch
+                elif branch_type == 'bug':
+                    b_id = r.sample(ascii_letters, 2)
+                    new_branch = f'bug-{b_id[0]}{b_id[1]}{r.randint(10, 99)}{r.choice(ascii_letters)}'
+                    if new_branch in self.bug_branches:
+                        continue
+                    else:
+                        self.bug_branches.append(new_branch)
+                        return new_branch
+            else:
+                return False
