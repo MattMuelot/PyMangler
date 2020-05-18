e4755fb07b67581172fc21ae45d67f93b9a62c51
MattMuelot <mattmuelot@gmail.com>
2020-05-18 07:51:32 -0700

    New project direction

--- main_manager.py
+++ main_manager.py
@@ -1,60 +1,15 @@
-import random as r
-from string import ascii_letters
+from pwd import PASSWORD
+import os
 
 
-class Mangler:
-    def __init__(self, proj_name):
-        self.proj_name = proj_name
-        self.feature_branches = []
-        self.bug_branches = []
-        self.accepted_branch_types = ['feature', 'bug']
+class SlaveDriver:
+    def __init__(self):
+        self.name = 'pi'
+        self.ip = '10.0.0.170'
+        self.password = PASSWORD
 
-    def __repr__(self):
-        return f"Mangler('{self.proj_name}')"
+    def ssh_in(self):
+        os.system(f'ssh {self.name}@{self.ip}')
 
-    def new_branch_name(self, branch_type):
-        while True:
-            if branch_type in self.accepted_branch_types:
-                if branch_type == 'feature':
-                    b_id = r.sample(ascii_letters, 2)
-                    new_branch = f'feature-{b_id[0]}{b_id[1]}{r.randint(10, 99)}{r.choice(ascii_letters)}'
-                    if new_branch in self.feature_branches:
-                        continue
-                    else:
-                        self.feature_branches.append(new_branch)
-                        return new_branch
-                elif branch_type == 'bug':
-                    b_id = r.sample(ascii_letters, 2)
-                    new_branch = f'bug-{b_id[0]}{b_id[1]}{r.randint(10, 99)}{r.choice(ascii_letters)}'
-                    if new_branch in self.bug_branches:
-                        continue
-                    else:
-                        self.bug_branches.append(new_branch)
-                        return new_branch
-            else:
-                return False
 
-    def get_features(self):
-        return self.feature_branches
-
-    def get_bugs(self):
-        return self.bug_branches
-
-    def purge_project(self):
-        for f in self.feature_branches:
-            self.feature_branches.remove(f)
-
-        for i in self.bug_branches:
-            self.bug_branches.remove(i)
-
-    def delete_branch_by_name(self, branch_name):
-        if 'feature' in branch_name:
-            if branch_name in self.feature_branches:
-                self.feature_branches.remove(branch_name)
-            else:
-                pass
-        elif 'bug' in branch_name:
-            if branch_name in self.bug_branches:
-                self.bug_branches.remove(branch_name)
-            else:
-                pass
+s = SlaveDriver()
